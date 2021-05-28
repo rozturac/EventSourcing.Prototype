2eff37963f7bd82309928f14cd9e13b7773aede9
Rıdvan <46276980+rozturac@users.noreply.github.com>
2021-05-28 13:50:00 +0300

    Initial commit

---
+++ Queries/Tests/EventSourcing.Prototype.Queries.Application.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace EventSourcing.Prototype.Queries.Application.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Queries/Tests/EventSourcing.Prototype.Queries.Domain.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace EventSourcing.Prototype.Queries.Domain.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
